b6e1085c72563ea3518752772af50b5c84afb037
artur <mauboroda.arturtu@gmail.com>
2024-10-07 21:21:56 +0300

    aq

--- practise/1.6/main.cpp
+++ practise/1.6/main.cpp
@@ -4,20 +4,24 @@
 #include <fstream>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 
+// Функція для додавання кольору до символу
 string colorize(char c, const string& colorCode) {
     return "\033[" + colorCode + "m" + c + "\033[0m";
 }
 
+// Функція для отримання випадкової іграшки
 char getRandomToy() {
-    string toys = "@$%#*";
+    string toys = "@$%#";
     return toys[rand() % toys.size()];
 }
 
-void drawTree(int n, ofstream &outfile, bool garlandOn, const string& colorCode) {
-    srand(time(0));
+// Функція для відображення ялинки
+void drawTree(int n, ofstream &outfile, bool garlandOn, vector<string>& colors) {
+    srand(time(0)); // Ініціалізація генератора випадкових чисел
 
     // Очищення консолі
     #ifdef _WIN32
@@ -26,27 +30,35 @@ void drawTree(int n, ofstream &outfile, bool garlandOn, const string& colorCode)
     system("clear");
     #endif
 
+    int maxWidth = 2 * n - 1; // Максимальна ширина ялинки
+
     for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n - i; ++j) {
+        // Виведення пробілів для вирівнювання трикутників
+        for (int j = 0; j < n - i; ++j) {
             cout << " ";
             outfile << " ";
         }
+
+        // Виведення зірочок та іграшок
         for (int j = 1; j <= 2 * i - 1; ++j) {
-            if (rand() % 5 == 0 && garlandOn) {
-                cout << colorize(getRandomToy(), colorCode);
-                outfile << colorize(getRandomToy(), colorCode);
+            if (rand() % 5 == 0 && garlandOn) { // Випадково визначаємо, чи буде гірлянда
+                string randomColor = colors[rand() % colors.size()]; // Випадковий колір
+                char toy = getRandomToy(); // Випадкова іграшка
+                cout << colorize(toy, randomColor); // Виведення кольорової іграшки
+                outfile << toy; // Запис в файл (без кольору, так як файл не підтримує кольори)
             } else {
-                cout << "*";
-                outfile << "*";
+                cout << "*"; // Виведення зірочки
+                outfile << "*"; // Запис в файл
             }
         }
+
         cout << endl;
         outfile << endl;
     }
 
-    // Стовбур
-    for (int i = 1; i <= n / 2; ++i) {
-        for (int j = 1; j <= n - 2; ++j) {
+    // Виведення стовбура
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < n - 2; ++j) {
             cout << " ";
             outfile << " ";
         }
@@ -56,25 +68,33 @@ void drawTree(int n, ofstream &outfile, bool garlandOn, const string& colorCode)
 }
 
 int main() {
-    int n, numFlashes;
-    string color = "32"; // Зелений колір (можна змінити)
+    srand(time(0)); // Ініціалізація генератора випадкових чисел
 
+    int n;
     cout << "Введіть кількість рівнів ялинки: ";
     cin >> n;
 
-    cout << "Введіть кількість мигань гірлянди: ";
-    cin >> numFlashes;
+    int numFlashes = 20; // Кількість разів, коли гірлянди будуть мигати
 
-    // ... (перевірка вводу)
+    // Ініціалізація кольорів (вектор без списку ініціалізації)
+    vector<string> colors;
+    colors.push_back("31"); // Червоний
+    colors.push_back("32"); // Зелений
+    colors.push_back("33"); // Жовтий
+    colors.push_back("34"); // Синій
+    colors.push_back("35"); // Пурпуровий
+    colors.push_back("36"); // Блакитний
 
-    ofstream outfile("yalynka.txt");
+    ofstream outfile("tree_output.txt"); // Відкриття файлу для запису
 
+    // Створення ефекту миготіння гірлянд
     for (int i = 0; i < numFlashes; ++i) {
-        bool garlandOn = (i % 2 == 0);
-        drawTree(n, outfile, garlandOn, color);
-        this_thread::sleep_for(chrono::milliseconds(500));
+        bool garlandOn = (i % 2 == 0); // Включення та виключення гірлянд
+        drawTree(n, outfile, garlandOn, colors); // Виведення ялинки з поточним станом гірлянд
+        this_thread::sleep_for(chrono::milliseconds(500)); // Затримка перед наступним оновленням
     }
 
-    outfile.close();
+    outfile.close(); // Закриття файлу після завершення запису
+
     return 0;
 }
