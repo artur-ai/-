ea0fcfe684e1c4157a1479986c00cfa194731d21
artur <mauboroda.arturtu@gmail.com>
2024-10-21 10:32:58 +0300

    bop

--- practise/2.3/main.cpp
+++ practise/2.3/main.cpp
@@ -8,53 +8,45 @@
 
 using namespace std;
 
-// Функція для додавання кольору до символу
 string colorize(char c, const string& colorCode) {
     return "\033[" + colorCode + "m" + c + "\033[0m";
 }
 
-// Функція для отримання випадкової іграшки
 char getRandomToy() {
     string toys = "@$%#";
     return toys[rand() % toys.size()];
 }
 
-// Функція для відображення ялинки
 void drawTree(int n, ofstream &outfile, bool garlandOn, vector<string>& colors) {
-    // Очищення консолі
     #ifdef _WIN32
-    system("cls");
     #else
     system("clear");
     #endif
 
-    int maxWidth = 2 * n - 1; // Максимальна ширина ялинки
+    int maxWidth = 2 * n - 1; 
 
     for (int i = 1; i <= n; ++i) {
-        // Виведення пробілів для вирівнювання трикутників
+        
         for (int j = 0; j < n - i; ++j) {
             cout << " ";
             outfile << " ";
         }
 
-        // Виведення зірочок та іграшок
         for (int j = 1; j <= 2 * i - 1; ++j) {
-            if (rand() % 5 == 0 && garlandOn) { // Випадково визначаємо, чи буде гірлянда
-                string randomColor = colors[rand() % colors.size()]; // Випадковий колір
-                char toy = getRandomToy(); // Випадкова іграшка
-                cout << colorize(toy, randomColor); // Виведення кольорової іграшки
-                outfile << toy; // Запис в файл (без кольору)
+            if (rand() % 5 == 0 && garlandOn) { 
+                string randomColor = colors[rand() % colors.size()];
+                char toy = getRandomToy();
+                cout << colorize(toy, randomColor);
+                outfile << toy;
             } else {
-                cout << "*"; // Виведення зірочки
-                outfile << "*"; // Запис в файл
+                cout << "*";
+                outfile << "*";
             }
         }
-
         cout << endl;
         outfile << endl;
     }
 
-    // Виведення стовбура
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < n - 2; ++j) {
             cout << " ";
@@ -66,33 +58,28 @@ void drawTree(int n, ofstream &outfile, bool garlandOn, vector<string>& colors) 
 }
 
 int main() {
-    srand(time(0)); // Ініціалізація генератора випадкових чисел
+    srand(time(0));
 
     int n;
     cout << "Введіть кількість рівнів ялинки: ";
     cin >> n;
 
-    int numFlashes = 20; // Кількість разів, коли гірлянди будуть мигати
-
-    // Ініціалізація кольорів (вектор без списку ініціалізації)
     vector<string> colors;
-    colors.push_back("31"); // Червоний
-    colors.push_back("32"); // Зелений
-    colors.push_back("33"); // Жовтий
-    colors.push_back("34"); // Синій
-    colors.push_back("35"); // Пурпуровий
-    colors.push_back("36"); // Блакитний
+    colors.push_back("31");
+    colors.push_back("32");
+    colors.push_back("33");
+    colors.push_back("34");
+    colors.push_back("35");
+    colors.push_back("36");
 
-    ofstream outfile("tree_output.txt"); // Відкриття файлу для запису
+    ofstream outfile("tree_output.txt");
 
-    // Створення ефекту миготіння гірлянд
-    for (int i = 0; i < numFlashes; ++i) {
-        bool garlandOn = (i % 2 == 0); // Включення та виключення гірлянд
-        drawTree(n, outfile, garlandOn, colors); // Виведення ялинки з поточним станом гірлянд
-        this_thread::sleep_for(chrono::milliseconds(500)); // Затримка перед наступним оновленням
+    while (true){
+        drawTree(n, outfile, true, colors);
+        this_thread::sleep_for(chrono::milliseconds(500));
     }
 
-    outfile.close(); // Закриття файлу після завершення запису
+    outfile.close();
 
     return 0;
 }
