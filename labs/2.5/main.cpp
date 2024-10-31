39ab07e4d52eea18da9cb6faf46d640e58f1e1e5
artur <mauboroda.arturtu@gmail.com>
2024-11-01 00:25:06 +0200

    dob

--- labs/2.5/main.cpp
+++ labs/2.5/main.cpp
@@ -1,33 +1,98 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
-double calculateFunction(double x) {
-    if (x >= 0) {
-        // Обчислення для x >= 0
-        return pow(x, 2) + 4 * pow(x, x) + 2;
-    } else {
-        // Обчислення для x < 0
-        double product = 1;
-        for (int i = 1; i <= 4; ++i) {
-            product *= (x + pow(x, i - 1));
+struct Node {
+    int data;         
+    Node* next;      
+};
+
+inline int processValue(int value) {
+    return value * 2;
+}
+
+Node* createFirstList(int numElements) {
+    Node* head = nullptr;
+    Node* current = nullptr;
+
+    for (int i = 0; i < numElements; ++i) {
+        int value;
+        cout << "Введіть значення для елемента " << (i + 1) << ": ";
+        cin >> value;
+
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = nullptr;
+
+        if (!head) {
+            head = newNode;
+            current = head;
+        } else {
+            current->next = newNode;
+            current = newNode;
         }
-        return 4 * product;
     }
+
+    return head;
+}
+
+Node* createSecondList(Node* firstListHead) {
+    Node* secondListHead = nullptr;
+    Node* current = nullptr;
+
+    Node* temp = firstListHead;
+    while (temp) {
+        int processedValue = processValue(temp->data);
+
+        Node* newNode = new Node;
+        newNode->data = processedValue;
+        newNode->next = nullptr;
+
+        if (!secondListHead) {
+            secondListHead = newNode;
+            current = secondListHead;
+        } else {
+            current->next = newNode;
+            current = newNode;
+        }
+
+        temp = temp->next;
+    }
+
+    return secondListHead;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "nullptr" << endl;
 }
 
 int main() {
-    double x_start = -5;
-    double x_end = 4;
-    double h = 0.25;
+    int numElements;
+    cout << "Введіть кількість елементів для першого списку: ";
+    cin >> numElements;
 
-    cout << "x\t\ty" << endl;
-    cout << "------------------------" << endl;
+    Node* firstList = createFirstList(numElements);
+    cout << "Перший список: ";
+    printList(firstList);
 
-    for (double x = x_start; x <= x_end; x += h) {
-        double y = calculateFunction(x);
-        cout << x << "\t\t" << y << endl;
+    Node* secondList = createSecondList(firstList);
+    cout << "Другий список (оброблені значення): ";
+    printList(secondList);
+
+    while (firstList) {
+        Node* temp = firstList;
+        firstList = firstList->next;
+        delete temp;
+    }
+
+    while (secondList) {
+        Node* temp = secondList;
+        secondList = secondList->next;
+        delete temp;
     }
 
     return 0;
